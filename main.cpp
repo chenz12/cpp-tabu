3dcca9a09d2bded22ce84e3c1bf352e9072b9908
chenz12 <thu1994chenz@163.com>
2016-04-05 16:37:01 +0800

    添加了生成随机初始解
    目前没有bug

--- main.cpp
+++ main.cpp
@@ -3,15 +3,26 @@
 #include <fstream>
 #include <iomanip>
 #include <sstream>
+#include <math.h>
+#include <time.h>
+
 using namespace std;
 
 
-struct solution{
+class solution{
+public:
+	double total_f;
+	bool feasible;
+	vector<int> trace;//the index of route which this request is assigned to
+
+
 	vector<double> f;
-	vector<int> route;
+	vector<vector<int>> route;
+
 };
 
 class raw{
+public:
 	int id;
 	double x;
 	double y;
@@ -19,27 +30,43 @@ class raw{
 	int q;
 	int e;
 	int l;
-public:
+
 	raw(double* a){
-		id = *a++;
+		id = (int)*a++;
 		x = *a++;
 		y = *a++;
-		d = *a++;
-		q = *a++;
-		e = *a++;
-		l = *a++;
+		d = (int)*a++;
+		q = (int)*a++;
+		e = (int)*a++;
+		l = (int)*a++;
 	}
 };
 
 class tabu{
 private:
-
+	//raw input
 	vector<raw> input;
 	int m;
 	int n;
 	int T;
 	int Q;
 	int L;
+	//processed data
+
+	vector<vector<double>> t;//travel time
+	vector<vector<double>> c;//distance
+
+	//para
+	double alpha;
+	double beta;
+	double gamma;
+	double tau;
+	double delta;
+	
+	double f;
+
+
+
 public:
 	tabu(void){
 		ifstream in("pr01", ios::in);
@@ -58,6 +85,18 @@ public:
 					input.push_back(raw(a));
 				}
 			}
+			initial_dist();
+	}
+	void initial_dist(){
+		for (int i = 0; i < 2 * n + 1;i++){
+			vector<double> tem;
+			for (int j = 0; j < 2 * n + 1;j++){
+				double te = sqrt(pow((input[i].x - input[j].x), 2) + pow((input[i].y - input[j].y), 2));
+				tem.push_back(te);
+			}
+			t.push_back(tem);
+			c.push_back(tem);
+		}
 	}
 
 	void initial_tabu(ifstream & a){
@@ -74,7 +113,7 @@ public:
 				i++;
 			}
 			m = te[0];
-			n = te[1];
+			n = te[1]/2;
 			T = te[2];
 			Q = te[3];
 			L = te[4];
@@ -83,11 +122,38 @@ public:
 			cout << "err in opening file";
 		
 	}
-	void 
+	//generate m routes ***  
+	solution gen_init_ramdon(){
+		solution s = solution();
+		s.trace.push_back(-1);
+		for (int i = 0; i < m ;i++){
+			vector<int> tem;
+			tem.push_back(0);
+			s.route.push_back(tem);
+			s.f.push_back(0);
+		}
+		srand((int)time(0));
+		for (int i = 1; i <=n;i++)
+		{
+			int randnum = int(rand())%m;
+			s.trace.push_back(randnum);
+			s.route[randnum].push_back(i);
+			s.route[randnum].push_back(i+n);
+		}
+		for (int i = 0; i < m;i++)
+		{
+			s.route[i].push_back(0);
+		}
+		return s;
+	}
 
 };
 
 void main(){
+
+	
 	tabu a = tabu();
+	solution t = a.gen_init_ramdon();
 	cout << 1;
-}
+	
+	}
