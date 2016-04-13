54cc722f4a4bd0d80623d289c484911adacf1e8c
chenz12 <thu1994chenz@163.com>
2016-04-13 22:23:58 +0800

    已经找到了一些问题，应该还有地方有问题
    发现了bug是由于C++里面的数组传值为新对象所导致的，而且之前写的copy函数可以删去了，大概就是这样

--- main.cpp
+++ main.cpp
@@ -7,18 +7,44 @@
 #include <time.h>
 
 using namespace std;
+
+
+class dpair{
+public:
+	int x;
+	int y;
+	dpair(int a, int b){
+		x = a;
+		y = b;
+	}
+
+};
 template<class T>
-int indexof(vector<T> a, T b){
+int tindexof(vector<T> a, T b){
 	for (int i = 0; i < a.size(); i++){
-		if (a[i]==b)
+		if (equal_pair(a[i],b))
 		{
 			return i;
 		}
 	}
 	return -1;
 }
+
+bool equal_pair(dpair a, dpair b){
+	if (a.x == b.x&&a.y == b.y){
+		return true;
+	}
+	return false;
+}
+bool equal_pair(int a, int b){
+	if (a==b){
+		return true;
+	}
+	return false;
+}
+
 template<class T>
-T max(T a, T b){
+T dmax(T a, T b){
 	if (a>b)
 	{
 		return a;
@@ -26,7 +52,7 @@ T max(T a, T b){
 	else return b;
 }
 template<class T>
-T min(T a, T b){
+T dmin(T a, T b){
 	if (a > b)
 	{
 		return b;
@@ -34,9 +60,29 @@ T min(T a, T b){
 	else return a;
 }
 template<class T>
-bool is_zero(T x){
+bool tis_zero(T x){
 	return x < 0.00001;
 }
+
+vector<int> tremove(vector<int> a, int i) {
+	for (vector<int>::iterator it = a.begin(); it != a.end(); it++) {
+		if (*it == i) {
+			it = a.erase(it);
+		}
+	}
+	return a;
+}
+
+template<class T>
+vector<T> routecopy(vector<T> a) {
+	vector<T> res;
+	for (vector<T>::iterator it = a.begin(); it != a.end(); it++) {
+		res.push_back(*it);
+	}
+	return res;
+}
+
+
 
 class route{
 public:
@@ -107,7 +153,7 @@ public:
 };
 
 class tabu{
-private:
+public:
 	//raw input
 	vector<raw> input;
 	int m;
@@ -129,9 +175,20 @@ private:
 	
 	double f;
 
+	//tabu list
+	vector<dpair> tabulist;
+
+	//rho
+	vector<vector<int>> rho;
+
+	//process parameter
+	bool bestfound = false;
+	double bestf;
+	solution bests;
 
 
-public:
+
+
 	tabu(void){
 		ifstream in("pr01", ios::in);
 		string inp;
@@ -195,6 +252,7 @@ public:
 			tem.push_back(0);
 			s.rout.push_back(tem);
 			s.f.push_back(0);
+			s.fea.push_back(false);
 		}
 		srand((int)time(0));
 		for (int i = 1; i <=n;i++)
@@ -222,7 +280,7 @@ public:
 
 	route route_evaluation(vector<int> a){
 		route ret = route();
-		for (int i = 0; i < a.size(); i++)
+		for (size_t i = 0; i < a.size(); i++)
 		{
 			ret.e.push_back(input[a[i]].e);
 			ret.l.push_back(input[a[i]].l);
@@ -241,10 +299,10 @@ public:
 		D[0] = B[0] + input[a[0]].d;
 		W[0] = 0;
 		
-		for (int i = 1; i < a.size();i++)
+		for (size_t i = 1; i < a.size();i++)
 		{
 			A[i] = D[i - 1] + t[a[i - 1]][a[i]];
-			B[i] = max(A[i], input[a[i]].e);
+			B[i] = dmax(A[i], input[a[i]].e);
 			D[i] = B[i] + input[a[i]].d;
 			W[i] = input[a[i]].e - A[i];
 			if (W[i]<0){
@@ -271,28 +329,28 @@ public:
 						//}
 						if (a[k] + n != a[i]){
 							_WSum = 0;
-							_F0 = min(_F0, L - ret.T[i] + _WSum);
+							_F0 = dmin(_F0, L - ret.T[i] + _WSum);
 						}
 					}
 				}
 					if (i == a.size() - 1){
 						_F0 = 0;
 					}
-					_F0 = WSum + max(0, _F0);
+					_F0 = WSum + dmax(0, _F0);
 					if (F0<0 || F0>_F0){
 						F0 = _F0;
 					}
 			}
 			A[j] += F0;
-			B[j] = max(A[j], input[a[j]].e);
+			B[j] = dmax(A[j], input[a[j]].e);
 			D[j] = B[j] + input[a[j]].d;
-			W[j] = max(0, input[a[j]].e - A[j]);
+			W[j] = dmax(0, input[a[j]].e - A[j]);
 			if (a[j] > 0 && a[j] <= n){
 				_D[a[j]] = D[j];
 			}
 			for (int i = j + 1; i < a.size(); i++){
 				A[i] = D[i - 1] + t[a[i - 1]][a[i]];
-				B[i] = max(A[i], input[a[i]].e);
+				B[i] = dmax(A[i], input[a[i]].e);
 				D[i] = B[i] + input[a[i]].d;
 				W[i] = input[a[i]].e - A[i];
 				if (W[i] < 0){
@@ -341,13 +399,13 @@ public:
 	}
 
 
-	void simple_insertion_1(vector<int> rout, int v){
+	void simple_insertion_1(vector<int> &rout, int v){
 		int va, vb;
 		int left = 1;
 		int right = rout.size() - 1;
 
-		va = indexof(rout, v - n);
-		vb = indexof(rout, v + n);
+		va = tindexof(rout, v - n);
+		vb = tindexof(rout, v + n);
 		if (v > n && va > -1)
 		{
 			left = va + 1;
@@ -356,14 +414,15 @@ public:
 		{
 			right = vb;
 		}
-		int m = 0, pos;
+		int m = 0, pos,f;
 		for (int i = left; i <= right; i++)
 		{
 			vector<int> te(rout.size());
-			copy(rout.begin(), rout.end(), te);
+			te=routecopy(rout);
 			te.insert(te.begin() + i, v);
-			double f = objective_f(te);
-			if (m > f){
+			f = objective_f(te);
+			
+			if (m > f||m==0){
 				m = f;
 				pos = i;
 			}
@@ -371,7 +430,7 @@ public:
 		rout.insert(rout.begin() + pos, v);
 	}
 
-	void simple_insertion_2(vector<int> rout, int v){
+	void simple_insertion_2(vector<int> &rout, int v){
 		int va, vb;
 		if (input[v + n].l - input[v + n].e > input[v].l - input[v].e){
 			va = v;
@@ -396,21 +455,21 @@ public:
 	}
 
 	bool is_fea_ret(route r){
-		return is_zero(r.q) && is_zero(r.d) && is_zero(r.w) && is_zero(r.t);
+		return tis_zero(r.q) && tis_zero(r.d) && tis_zero(r.w) && tis_zero(r.t);
 	}
 
-	double total_f(solution s){
+	double total_f(solution &s){
 		double f = 0;
 		for (int i = 0; i < s.rout.size(); i++){
 			route r = route_evaluation(s.rout[i]);
-			s.fea.push_back(is_fea_ret(r));
-			s.f.push_back(objective_f(r));
+			s.fea[i]=is_fea_ret(r);
+			s.f[i]=objective_f(r);
 			f += s.f[i];
 		}
 		return f;
 	}
 
-	void solve(){
+	void tsolve(){
 		alpha = 10;
 		beta = 0.1;
 		gamma = 1;
@@ -421,9 +480,146 @@ public:
 
 		int iter_best, iter_end = 400;
 		int iter_internal = 10;
-		
+		int theta = iter_end / 40;
+		int lambda = 10;
+		for (int i = 0; i < n+1; i++){
+			vector<int> tem(m);
+			for (int j = 0; j < m; j++){
+				tem[j] = 0;
+			}
+			rho.push_back(tem);
+		}
+
+		for (int iter = 0; iter < iter_end; iter++){
+			f = total_f(s);
+			double minf = 10000000000;
+			int mini = -1;
+			int minj = -1;
+			double newf;
+
+			//bool dbg;
+
+			for (int i = 1; i <= n; i++){
+				for (int j = 0; j < m; j++){
+					if (j == s.trace[i]){
+						continue;
+					}
+					dpair ta = dpair(i, j);
+					int k0 = s.trace[i];
+					int k1 = j;
+					vector<int>* s0 = &s.rout[k0];
+					vector<int>* s1 = &s.rout[k1];
+					double f00 = s.f[k0];
+					double f10 = s.f[k1];
+					vector<int> r0 = routecopy(*s0);
+					vector<int> r1 = routecopy(*s1);
+					tremove(r0, i);
+					tremove(r0, i + n);
+					simple_insertion_2(r1, i);
+					double f01 = objective_f(r0);
+					double f11 = objective_f(r1);
+					newf = f01 + f11 - f00 - f10 + rho[i][j] * lambda;
+					cout << "i:" << i << " j:" << j << " newf: " << newf << " minf:" << minf << endl;
+					cout << " f01:" << f01 << " f11:" << f11 << " f00" << f00 << " f10" << f10 << " rho:" << rho[i][j] << endl;
+					if (tindexof(tabulist, ta) > -1 && f + newf > bestf){
+						//dbg = true;
+						continue;
+					}
+
+					if (minf > newf){
+						minf = newf;
+						mini = i;
+						minj = j;
+					}
+				}
+			}
+			if (mini == -1&&minj==-1){
+				cout << "buging" << endl;
+			//	continue;
+			}
+			int i = mini;
+			int j = minj;
+			rho[i][j]++;
+			int k0 = s.trace[i];
+			int k1 = j;
+			vector<int>* s0 = &s.rout[k0];
+			vector<int>* s1 = &s.rout[k1];
+			tremove(*s0, i);
+			tremove(*s0, i + n);
+			simple_insertion_2(*s1, i);
+			s.trace[i] = j;
+			dpair ta = dpair(i, k0);
+			if (tindexof(tabulist, ta) == -1){
+				tabu_push(tabulist, ta, theta);
+			}
+			if (iter%iter_internal == 0){
+				cout << "internal iter"<<endl;
+				for (int k = 1; k <=n; k++){
+					vector<int> ss = s.rout[s.trace[k]];
+					tremove(ss, k);
+					tremove(ss, k + n);
+					simple_insertion_2(ss, k);
+				}
+			}
+			f = total_f(s);
+			bool fea = is_fea(s);
+			double ratio = 1 + delta;
+			if (fea){
+				ratio = 1 / ratio;
+				if (!bestfound || bestf>f){
+					bestfound = true;
+					bestf = f;
+					//time_best = time();
+
+					iter_best = iter;
+					cout << "better f" << f << endl;
+
+				}
+			}
+			alpha *= ratio;
+			beta *= ratio;
+			gamma *= ratio;
+			tau *= ratio;
+		}
+
+		//time_end = time();
+		if (bestfound){
+			double dura = 0;
+			double waiting = 0;
+			double transit = 0;
+			for (int i = 0; i < s.rout.size(); i++){
+				route ret = route_evaluation(s.rout[i]);
+				dura += ret.duration;
+				waiting += dsum(ret.W);
+				transit += dsum(ret.T);
+			}
+			cout << "f:" << bestf << endl;
+			cout << "duration:" << dura << endl;
+			cout << "waiting:" << waiting << endl;
+			cout << "transit:" << transit << endl;
+		}
 	}
 
+	int dsum(vector<int> a){
+		int res = 0;
+		for (vector<int>::iterator it = a.begin(); it != a.end(); it++){
+			res += *it;
+		}
+		return res;
+	}
+
+	void tabu_push(vector<dpair> &a, dpair b, int c){
+		if (a.size() < c){
+			a.push_back(b);
+		}
+		else if (a.size() == c){
+			a.erase(a.begin());
+			a.push_back(b);
+		}
+		else {
+			cout << "error";
+		}
+	}
 };
 
 void main(){
@@ -431,7 +627,14 @@ void main(){
 	
 	tabu a = tabu();
 	solution t = a.gen_init_ramdon();
+	a.tsolve();
+	
+	/*for (int i = 0; i < 20; i++){
+		cout << "cost£º" << a.objective_f(a.route_evaluation(t.rout[0])) << endl;
+		cout << "feasible: " << a.is_fea_ret(a.route_evaluation(t.rout[0])) << endl;
+	}*/
 	cout << 1;
+	system("pause");
 	/*
 	vector<int> myints = { 10, 20, 30, 40 };
 	int i;
